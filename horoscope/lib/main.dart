e547a03d5e619b07eae78b4407af1b51c52b9968
dmitry <stanko.11@mail.ru>
2023-03-25 22:52:42 +0700

    Closes #5; sprint 1.0.3 completed 5 page.

--- horoscope/lib/main.dart
+++ horoscope/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:readmore/readmore.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +22,10 @@ class MyApp extends StatelessWidget {
             titleSmall: TextStyle(
                 color: Colors.black, fontSize: 16, fontWeight: FontWeight.w500),
           )),
-      home: const FadeThroughTransitionHoroscope(),
+      routes: {
+        '/': (context) => const FadeThroughTransitionHoroscope(),
+        '/finder/map': (context) => const _MapScreem()
+      },
     );
   }
 }
@@ -194,11 +198,423 @@ class _LovePage extends StatelessWidget {
 }
 
 class _FinderPage extends StatelessWidget {
+  // final _formsList = <Widget>[
+  //   _OldForm(),
+  //   _OldForm(),
+  //   _OldForm(),
+  //   _OldForm(),
+  //   _OldForm()
+  // ];
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('go to finder'),
+    return SingleChildScrollView(
+      child: Container(
+          color: Colors.indigo[800],
+          child: Form(
+              child: Column(children: [
+            Container(
+                decoration: BoxDecoration(
+                    border: Border.all(width: 0, color: Colors.white),
+                    borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10)),
+                    color: Colors.white),
+                margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+                alignment: Alignment.topLeft,
+                child: const Text(
+                    'Здесь вы можете искать подходящих вам по гороскопу людей, напишите кого вы желаете найти')),
+            Container(
+                decoration: BoxDecoration(
+                    border: Border.all(width: 0, color: Colors.white),
+                    color: Colors.white),
+                alignment: Alignment.topLeft,
+                padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Возраст',
+                          style: Theme.of(context).textTheme.titleSmall),
+                      _AgeForm(),
+                      Text('Пол',
+                          style: Theme.of(context).textTheme.titleSmall),
+                      const GenderRadios(),
+                      Text('Создвездие',
+                          style: Theme.of(context).textTheme.titleSmall),
+                      const DropdownButtonConstellation(),
+                      Text('Радиус поиска',
+                          style: Theme.of(context).textTheme.titleSmall),
+                      const ButtonsRadioForm(),
+                      _ButtonSearch()
+                    ]))
+          ]))),
     );
+  }
+}
+// Container(
+//         color: Colors.indigo[800],
+//         child: Form(
+//             child: Column(children: [
+//           Container(
+//               decoration: const BoxDecoration(
+//                   borderRadius: BorderRadius.only(
+//                       topLeft: Radius.circular(10),
+//                       topRight: Radius.circular(10)),
+//                   color: Colors.white),
+//               margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+//               padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+//               alignment: Alignment.topLeft,
+//               child: const Text(
+//                   'Здесь вы можете искать подходящих вам по гороскопу людей, напишите кого вы желаете найти')),
+//           Expanded(
+//               child: Container(
+//                   color: Colors.white,
+//                   alignment: Alignment.topLeft,
+//                   padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
+//                   child: Column(
+//                       crossAxisAlignment: CrossAxisAlignment.start,
+//                       children: [
+//                         Text('Возраст',
+//                             style: Theme.of(context).textTheme.titleSmall),
+//                         _AgeForm(),
+//                         Text('Пол',
+//                             style: Theme.of(context).textTheme.titleSmall),
+//                         const GenderRadios(),
+//                         Text('Создвездие',
+//                             style: Theme.of(context).textTheme.titleSmall),
+//                         const DropdownButtonConstellation(),
+//                         Text('Радиус поиска',
+//                             style: Theme.of(context).textTheme.titleSmall),
+//                         const ButtonsRadioForm(),
+//                         _ButtonSearch()
+//                       ])))
+//         ])))
+
+class _AgeForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.only(top: 15, bottom: 40),
+        height: 35,
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                style: Theme.of(context).textTheme.titleSmall,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'От'),
+              ),
+            ),
+            Flexible(
+              child: TextField(
+                style: Theme.of(context).textTheme.titleSmall,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'До'),
+              ),
+            )
+          ],
+        ));
+  }
+}
+
+enum Gender { male, female }
+
+class GenderRadios extends StatefulWidget {
+  const GenderRadios({super.key});
+
+  @override
+  State<GenderRadios> createState() => _GenderRadios();
+}
+
+class _GenderRadios extends State<GenderRadios> {
+  Gender? _gender = Gender.male;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.only(top: 10, bottom: 40),
+        height: 35,
+        child: Row(
+          children: <Widget>[
+            const Flexible(
+              child: Text('Мужчина'),
+            ),
+            Radio<Gender>(
+                value: Gender.male,
+                groupValue: _gender,
+                onChanged: (Gender? value) {
+                  setState(() {
+                    _gender = value;
+                  });
+                }),
+            const Flexible(
+              child: Text('Женщина'),
+            ),
+            Radio<Gender>(
+                value: Gender.female,
+                groupValue: _gender,
+                onChanged: (Gender? value) {
+                  setState(() {
+                    _gender = value;
+                  });
+                }),
+          ],
+        ));
+  }
+}
+
+class DropdownButtonConstellation extends StatefulWidget {
+  const DropdownButtonConstellation({super.key});
+
+  @override
+  State<DropdownButtonConstellation> createState() =>
+      _DropdownButtonConstellationState();
+}
+
+class CustomDropdownButton2 extends StatelessWidget {
+  final String hint;
+  final String? value;
+  final List<String> dropdownItems;
+  final ValueChanged<String?>? onChanged;
+  final DropdownButtonBuilder? selectedItemBuilder;
+  final Alignment? hintAlignment;
+  final Alignment? valueAlignment;
+  final double? buttonHeight, buttonWidth;
+  final EdgeInsetsGeometry? buttonPadding;
+  final BoxDecoration? buttonDecoration;
+  final int? buttonElevation;
+  final Widget? icon;
+  final double? iconSize;
+  final Color? iconEnabledColor;
+  final Color? iconDisabledColor;
+  final double? itemHeight;
+  final EdgeInsetsGeometry? itemPadding;
+  final double? dropdownHeight, dropdownWidth;
+  final EdgeInsetsGeometry? dropdownPadding;
+  final BoxDecoration? dropdownDecoration;
+  final int? dropdownElevation;
+  final Radius? scrollbarRadius;
+  final double? scrollbarThickness;
+  final bool? scrollbarAlwaysShow;
+  final Offset offset;
+
+  const CustomDropdownButton2({
+    required this.hint,
+    required this.value,
+    required this.dropdownItems,
+    required this.onChanged,
+    this.selectedItemBuilder,
+    this.hintAlignment,
+    this.valueAlignment,
+    this.buttonHeight,
+    this.buttonWidth,
+    this.buttonPadding,
+    this.buttonDecoration,
+    this.buttonElevation,
+    this.icon,
+    this.iconSize,
+    this.iconEnabledColor,
+    this.iconDisabledColor,
+    this.itemHeight,
+    this.itemPadding,
+    this.dropdownHeight,
+    this.dropdownWidth,
+    this.dropdownPadding,
+    this.dropdownDecoration,
+    this.dropdownElevation,
+    this.scrollbarRadius,
+    this.scrollbarThickness,
+    this.scrollbarAlwaysShow,
+    this.offset = const Offset(0, 0),
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButtonHideUnderline(
+      child: DropdownButton2(
+        //To avoid long text overflowing.
+        isExpanded: true,
+        hint: Container(
+          alignment: hintAlignment,
+          child: Text(
+            hint,
+            overflow: TextOverflow.ellipsis,
+            maxLines: 1,
+            style: TextStyle(
+              fontSize: 14,
+              color: Theme.of(context).hintColor,
+            ),
+          ),
+        ),
+        value: value,
+        items: dropdownItems
+            .map((item) => DropdownMenuItem<String>(
+                  value: item,
+                  child: Container(
+                    alignment: valueAlignment,
+                    child: Text(
+                      item,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                      style: const TextStyle(fontSize: 14, color: Colors.black),
+                    ),
+                  ),
+                ))
+            .toList(),
+        onChanged: onChanged,
+        selectedItemBuilder: selectedItemBuilder,
+        buttonStyleData: ButtonStyleData(
+          height: buttonHeight ?? 40,
+          width: buttonWidth ?? 140,
+          padding: buttonPadding ?? const EdgeInsets.only(left: 14, right: 14),
+          decoration: buttonDecoration ??
+              BoxDecoration(
+                borderRadius: BorderRadius.circular(14),
+                border: Border.all(
+                  color: Colors.black45,
+                ),
+              ),
+          elevation: buttonElevation,
+        ),
+        iconStyleData: IconStyleData(
+          icon: icon ?? const Icon(Icons.arrow_forward_ios_outlined),
+          iconSize: iconSize ?? 12,
+          iconEnabledColor: iconEnabledColor,
+          iconDisabledColor: iconDisabledColor,
+        ),
+        dropdownStyleData: DropdownStyleData(
+          //Max height for the dropdown menu & becoming scrollable if there are more items. If you pass Null it will take max height possible for the items.
+          maxHeight: dropdownHeight ?? 200,
+          width: dropdownWidth ?? 140,
+          padding: dropdownPadding,
+          decoration: dropdownDecoration ??
+              BoxDecoration(
+                borderRadius: BorderRadius.circular(14),
+              ),
+          elevation: dropdownElevation ?? 8,
+          //Null or Offset(0, 0) will open just under the button. You can edit as you want.
+          offset: offset,
+          //Default is false to show menu below button
+          isOverButton: false,
+          scrollbarTheme: ScrollbarThemeData(
+            radius: scrollbarRadius ?? const Radius.circular(40),
+            thickness: scrollbarThickness != null
+                ? MaterialStateProperty.all<double>(scrollbarThickness!)
+                : null,
+            thumbVisibility: scrollbarAlwaysShow != null
+                ? MaterialStateProperty.all<bool>(scrollbarAlwaysShow!)
+                : null,
+          ),
+        ),
+        menuItemStyleData: MenuItemStyleData(
+          height: itemHeight ?? 40,
+          padding: itemPadding ?? const EdgeInsets.only(left: 14, right: 14),
+        ),
+      ),
+    );
+  }
+}
+
+class _DropdownButtonConstellationState
+    extends State<DropdownButtonConstellation> {
+  final List<String> items = [
+    'Овен',
+    'Телец',
+    'Близнецы',
+    'Рак',
+    'Лев',
+    'Дева',
+    'Весы',
+    'Скорпион',
+    'Стрелец',
+    'Козерог',
+    'Водолей',
+    'Рыбы'
+  ];
+  String? selectedValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 15, bottom: 40),
+      child: CustomDropdownButton2(
+        hint: 'Выберите ',
+        dropdownItems: items,
+        value: selectedValue,
+        onChanged: (value) {
+          setState(() {
+            selectedValue = value;
+          });
+        },
+      ),
+    );
+    //   return DropdownButton<String>(
+    //     value: dropdownValue,
+    //     icon: const Icon(
+    //       Icons.arrow_downward,
+    //       size: 18,
+    //     ),
+    //     elevation: 16,
+    //     style: const TextStyle(color: Colors.deepPurple),
+    //     underline: Container(
+    //       height: 2,
+    //       color: Colors.deepPurpleAccent,
+    //     ),
+    //     onChanged: (String? value) {
+    //       // This is called when the user selects an item.
+    //       setState(() {
+    //         dropdownValue = value!;
+    //       });
+    //     },
+    //     items: list.map<DropdownMenuItem<String>>((String value) {
+    //       return DropdownMenuItem<String>(
+    //         value: value,
+    //         child: Text(value),
+    //       );
+    //     }).toList(),
+    //   );
+    // }
+  }
+}
+
+class ButtonsRadioForm extends StatefulWidget {
+  const ButtonsRadioForm({super.key});
+  @override
+  State<ButtonsRadioForm> createState() => _ButtonsRadioFormState();
+}
+
+/// This is the private State class that goes with ButtonsRadioForm.
+class _ButtonsRadioFormState extends State<ButtonsRadioForm> {
+  int value = 0;
+  Widget customRadioButton(String text, int index) {
+    return OutlinedButton(
+      onPressed: () {
+        setState(() {
+          value = index;
+        });
+      },
+      child: Text(
+        text,
+        style: TextStyle(
+          color: (value == index) ? Colors.blue : Colors.black,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.only(top: 10, bottom: 20),
+        child: Row(
+          children: <Widget>[
+            Expanded(child: customRadioButton("100 м", 1)),
+            Expanded(child: customRadioButton("500 м", 2)),
+            Expanded(child: customRadioButton("1 км", 3))
+          ],
+        ));
   }
 }
 
@@ -211,11 +627,38 @@ class _ProfilePage extends StatelessWidget {
   }
 }
 
+class _ButtonSearch extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Row(children: [
+      Expanded(
+          child: ElevatedButton(
+        onPressed: () {
+          Navigator.of(context).pushNamed('/finder/map');
+        },
+        child: const Text('Поиск'),
+      )),
+    ]));
+  }
+}
+
 class _SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Center(
       child: Text('go to settings'),
+    );
+  }
+}
+
+class _MapScreem extends StatelessWidget {
+  const _MapScreem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Map screen')),
     );
   }
 }
