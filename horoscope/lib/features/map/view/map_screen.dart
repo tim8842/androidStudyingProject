89984620ff675502c26f683f6e3eec9f9db475d8
dmitry <stanko.11@mail.ru>
2023-03-27 23:41:34 +0700

    sprint 1.1.0 add decomposed map, page 6

--- horoscope/lib/features/map/view/map_screen.dart
+++ horoscope/lib/features/map/view/map_screen.dart
@@ -1,20 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class MapScreen extends StatelessWidget {
   const MapScreen({super.key});
 
-  static const LatLng sourceLocation = LatLng(37.33, -122.032);
-  static const LatLng destination = LatLng(37.3342, -122.066);
+  // static const LatLng sourceLocation = LatLng(37.33, -122.032);
+  // static const LatLng destination = LatLng(37.3342, -122.066);
 
   @override
   Widget build(BuildContext context) {
+    final point = LatLng(55.994333, 92.797481);
     return Scaffold(
-      appBar: AppBar(title: const Text('Карта')),
-      body: Image.asset('assets/png/map.png'),
-      // body: const GoogleMap(
-      //     initialCameraPosition:
-      //         CameraPosition(target: sourceLocation, zoom: 14.5)),
-    );
+        appBar: AppBar(title: const Text('Карта')),
+        body: Stack(
+          children: [
+            FlutterMap(
+              options:
+                  MapOptions(center: LatLng(55.994333, 92.797481), zoom: 16.0),
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  subdomains: const ['a', 'b', 'c'],
+                ),
+                MarkerLayer(
+                  markers: [
+                    Marker(
+                        point: point,
+                        builder: (ctx) => const Icon(
+                              Icons.location_on,
+                              color: Colors.red,
+                            ))
+                  ],
+                )
+              ],
+            ),
+          ],
+        )
+        // body: Image.asset('assets/png/map.png'),
+        // body: const GoogleMap(
+        //     initialCameraPosition:
+        //         CameraPosition(target: sourceLocation, zoom: 14.5)),
+        );
   }
 }
