637a932ac842ed29bceae169ed9b0d457b9c1b27
dmitry <stanko.11@mail.ru>
2023-04-26 10:14:08 +0700

    add firebase

--- horoscope/lib/main.dart
+++ horoscope/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'horoscope_app.dart';
@@ -6,6 +7,7 @@ int initScreen = 0;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   SharedPreferences prefs = await SharedPreferences.getInstance();
   initScreen = (prefs.getInt("initScreen")) ?? 0;
   // await prefs.setInt("initScreen", 0);
